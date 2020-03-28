8f5122126dc61a11b93bea8a219d5a72dd708a44
dradojkovic <djordje.radojkovic@computerrock.com>
2020-03-28 20:47:02 +0100

    Add redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 
+// @ts-ignore
+import store from './store';
 import App from './App';
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
 
